c6e9bdc35567dce06abb6b6358a022a1d32a31ba
kirya <cloudkserg11@gmail.com>
2018-06-26 04:06:24 +0300

    https://chronobank.atlassian.net/browse/MD-304 - middleware check bot - create platform factory for all blockchains and alerter

--- index.js
+++ index.js
@@ -1,45 +1,132 @@
-const WavesAPI = require('@waves/waves-api'),
-request = require('request-promise');
+/* 
+* Copyright 2017–2018, LaborX PTY
+* Licensed under the AGPL Version 3 license.
+* @author Kirill Sergeev <cloudkserg11@gmail.com>
+*/
 
 
-const main = async () => {
-    const Waves = WavesAPI.create({
-        networkByte: 'TESTNET',
-        nodeAddress: 'bla-bla-bla',
-        matcherAddress: 'bla-bla',
-        minimumSeedLength: 1
-      });
-      const seed = Waves.Seed.fromExistingPhrase('advice shed boat scan game expire reveal rapid concert settle before vital');
-      const transferData = {
-        senderPublicKey: seed.keyPair.publicKey,
-        // An arbitrary address; mine, in this example
-        recipient: '3MuQHu64HwboRdxzh9rpPDuUJDR9UEHH7zE',
-        // ID of a token, or WAVES
-        assetId: 'WAVES',
-        // The real amount is the given number divided by 10^(precision of the token)
-        amount: 10000000,
-        // The same rules for these two fields
-        feeAssetId: 'WAVES',
-        fee: 100000,
-        // 140 bytes of data (it's allowed to use Uint8Array here)
-        attachment: '',
-        timestamp: Date.now()
-      };
-      console.log(Waves);
-      const Transactions = Waves.Transactions;
-      const transferTransaction = new Transactions.TransferTransaction(transferData);
-      const tx  = await transferTransaction.prepareForAPI(seed.keyPair.privateKey);
-      console.log(tx);
+const config = require('./config'),
+ Promise = require('bluebird'),
+ mongoose = require('mongoose');
 
-      const newTx = await request({
-        method: 'POST',
-        'json': true,
-        body: {tx},
-        uri: 'https://middleware-waves-testnet-rest.chronobank.io/tx/send'
-      });
-      console.log(newTx);
+mongoose.Promise = Promise; // Use custom Promises
+mongoose.connect(config.mongo.data.uri, {useMongoClient: true});
+mongoose.accounts = mongoose.createConnection(config.mongo.accounts.uri);
+
+const _ = require('lodash'),
+  bunyan = require('bunyan'),
+  amqp = require('amqplib'),
+  log = bunyan.createLogger({name: 'check-bot'}),
+  blockchainFactory = require('./services/blochainFactory'),
+  Alerter = require('./services/Alerter');
+
+
+let amqpInstance;
+
+[mongoose.accounts, mongoose.connection].forEach(connection =>
+  connection.on('disconnected', function () {
+    log.error('mongo disconnected!');
+    process.exit(0);
+  })
+);
+
+const registerAccounts = async (blockchain, accounts) => {
+  await Promise.mapSeries(accounts, async account => {
+    await blockchain.registerAccount(account);
+  });
 };
 
-main();
+const getInitBalances = async (blockchain, accounts) => {
+  return await Promise.mapSeries(accounts, async account => 
+    await blockchain.getBalance(account)
+  );
+};
 
+const getOneMessageAboutTransaction = async (callback) => {
+  const channel = await amqpInstance.createChannel();
+  await channel.assertExchange('internal', 'topic', {durable: false});
+  await channel.assertQueue(`${config.rabbit.serviceName}_test.block`);
+  await channel.bindQueue(`${config.rabbit.serviceName}_test.block`, 'internal', 
+    `${config.rabbit.serviceName}_transaction.*`
+  );
+  await new Promise(res => channel.consume(`${config.rabbit.serviceName}_test.block`, async (message) => {
+    const content = JSON.parse(message.content);
+    await callback(content);
+    await channel.cancel(message.fields.consumerTag);
+  }, {noAck: true}));
+};
 
+const getConfirmedMessage = (account, balance) => 
+'check changed balance after confirmed ' + account.address + 
+' balance ' + balance;
+
+const init = async () => {
+  amqpInstance = await amqp.connect(config.rabbit.url)
+    .catch(() => {
+      log.error('rabbitmq process has finished!');
+      process.exit(0);
+    });
+
+  const blockchains = blockchainFactory.create(config.blockchains);
+
+  await Promise.mapSeries(blockchains, blockchain => {
+    const alerter = new Alerter(blockchain);
+    await alerter.init();
+
+    const accounts = blockchain.getAccounts();
+    registerAccounts(blockchain, accounts);
+    alerter.info('register accounts');
+
+    const initBalances = getInitBalances(blockchain, accounts);
+    alerter.info('get init balances for accounts');    
+
+    let tx;
+    await Promise.all([
+      (async () => {
+        tx = blockchain.sendTransferTransaction(
+          accounts[0], accounts[1], 
+          blockchain.getTransferAmount()
+        );
+        alerter.info('send transfer transaction ' + tx.id);
+      })(),
+      (async () => {
+        
+        await getOneMessageAboutTransaction(async content => {
+          await blockchain.checkUnconfirmedTx(tx, content);
+        });
+        alerter.info('check unconfirmed transaction ' + tx.id);
+        
+        alerter.expect(
+          await blockchain.getBalance(accounts[0]), 
+          initBalances[0],
+          'check not changed balances after unconfirmed ' + tx.id
+        );
+        alerter.expect(
+          await blockchain.getBalance(accounts[1]), 
+          initBalances[1],
+          'check not changed balances after unconfirmed ' + tx.id
+        );
+      })(),
+      (async () => {
+        await getOneMessageAboutTransaction(async content => {
+          await blockchain.checkConfirmedTx(tx, content);
+        });
+        alerter.info('check confirmed transaction ' + tx.id);
+
+        alerter.expect(
+          await blockchain.getBalance(accounts[0]), 
+          initBalances[0]-blockchain.getTransferAmount(),
+          getConfirmedMessage(accounts[0], initBalances[0]-blockchain.getTransferAmount())
+        );
+        alerter.expect(
+          await blockchain.getBalance(accounts[0]), 
+          initBalances[0]+blockchain.getTransferAmount(),
+          getConfirmedMessage(accounts[1], initBalances[1]+blockchain.getTransferAmount())
+        );
+      })()
+    ]);
+  });
+
+};
+
+module.exports = init();
